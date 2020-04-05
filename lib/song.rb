0187b79603d38f3706d220d2620191fcb1d14d2b
Joshua Williams <jmw0426@hotmail.com>
2020-04-05 21:29:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,20 @@
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name, :artist_name, :song
   @@all = []
-
+  
   def self.all
     @@all
   end
+
+  def self.create(song)
+    song = Song.create
+    if !Song.all.include?(song)
+      @@all << song
+    end
+  end
+
+  def self.new_by_name
+    
 
   def save
     self.class.all << self
